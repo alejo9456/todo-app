25d916034f3e73526a4a6b216a367a18a5878ab8
Alejandro Villanueva <alejonc15@gmail.com>
2023-10-19 23:07:33 -0500

    feat: Change background for light version on desktop and mobile device

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
-import { useState } from 'react';
-import lightBackgroundImage  from './assets/bg-desktop-light.jpg';
+import { useEffect, useState } from 'react';
+import lightDesktopBackgroundImage  from './assets/bg-desktop-light.jpg';
+import lightMobileBackgroundImage  from './assets/bg-mobile-light.jpg';
 import { Search, TodoList } from './components';
 
 
@@ -36,10 +37,29 @@ export const App = () => {
       name: 'Complete Todo App on Frontend Mentor',
       complete: false,
     },
-  ])
+  ]);
+
+  const [isMobile, setIsMobile] = useState(false);
 
   const [filter, setFilter] = useState('all');
 
+  const [isAllComplete, setIsAllComplete] = useState(false);
+
+
+  useEffect(() => {
+    
+    const handleResize = () =>{
+      setIsMobile(window.innerWidth < 480);
+    }
+    
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    }
+  }, [])
+  
   const handleFilterChange = (newFilter) => {
     setFilter(newFilter);
   }
@@ -58,8 +78,27 @@ export const App = () => {
       todo.id === todoId ? updatedTodo : todo
     );
     setTodos(updatedTodos);
+    const isCompleteTodos = updatedTodos.every( todo => todo.complete);
+    setIsAllComplete(isCompleteTodos);
   };
+
+
+  const changeAllTodos = ( complete ) => {
+    const updatedTodos = todos.map((todo) => ({ ...todo, complete }));
+    setTodos(updatedTodos);
+    setIsAllComplete(complete);
+  }
+
   
+  const clearComplete = () => {
+    const updatedTodos = todos.filter( todo => !todo.complete);
+    setTodos(updatedTodos);
+    setIsAllComplete(false); 
+  }
+
+  const countIncompleteTodos = () => {
+    return todos.filter(todo => !todo.complete).length;
+  }
 
   const filteredTodos = todos.filter( (todo) => {
     if( filter === 'active'){
@@ -74,20 +113,29 @@ export const App = () => {
     <main className="h-screen relative bg-very-light-gray">
 
       <section className='h-40vh relative flex justify-center items-center'>
-        <img className="h-full w-full absolute object-cover object-center" src={lightBackgroundImage} alt="image" />
-        {/* Search */}
-        <Search onAddTodo = {addTodo }/>
+        <img 
+          className="h-full w-full absolute object-cover object-center"
+          src={ isMobile? lightMobileBackgroundImage : lightDesktopBackgroundImage } alt="image-background" />
       </section>
 
       <section className="bg-very-light-gray h-60vh relative flex flex-col items-center">
-
-        {/* { TodoList} */}
-        <TodoList todos= {filteredTodos} onDeleteTodo = { deleteTodo } onFilterChange={handleFilterChange} onUpdateTodo={updateTodo} />
-
-        <footer>
-          Drag and drog to reorder list
-        </footer>
-    
+        <div className='relative transform translate-y-[-11rem] max-w-[21rem] sm:min-w-[30rem]'>
+          <Search 
+              onAddTodo = {addTodo } 
+              onChangeAllTodos = { changeAllTodos}
+              isAllComplete={isAllComplete} />
+          <TodoList 
+            todos= {filteredTodos}
+            onDeleteTodo = { deleteTodo }
+            onFilterChange={handleFilterChange} 
+            onUpdateTodo={updateTodo}
+            onClearComplete={ clearComplete }
+            onIncompleteTodos={countIncompleteTodos}
+          />
+          <footer className='text-dark-grayish-blue flex justify-center'>
+            Drag and drog to reorder list
+          </footer>
+        </div>    
       </section>
 
     </main>
