8814b1fa9cd3e462b71959b67156da8dbe69c7fe
Alejandro Villanueva <alejonc15@gmail.com>
2023-10-19 23:14:59 -0500

    feat: Enhance TodoItem interaction with hover and focus for delete button

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,13 +1,14 @@
-    import { useState } from 'react'
+    import { useState } from 'react';
     import check from '../assets/icon-check.svg'
     import cross from '../assets/icon-cross.svg'
 
     export const TodoItem = ({ todo, onDeleteTodo, onUpdateTodo  }) => {
-        const [isComplete, setIsComplete] = useState(todo.complete);
+
+        const [isFocused, setIsFocused] = useState(false);
+        const [isHovered, setIsHovered] = useState(false);
 
         const handleCheckboxChange = () => {
-            const updatedTodo = {...todo, complete: !isComplete};
-            setIsComplete(!isComplete)
+            const updatedTodo = {...todo, complete: !todo.complete};
             onUpdateTodo(todo.id, updatedTodo)
         }
 
@@ -16,30 +17,39 @@
         }
 
         return (
-            <li className="flex items-center w-full bg-white rounded-t-lg px-5 py-2 border-b">
+            <li className="flex items-center w-full bg-white rounded-t-lg px-5 py-2 border-b"
+                onFocus={ ()=> setIsFocused(true)}
+                onBlur={ ()=> setIsFocused(false)}
+                onMouseEnter={()=> setIsHovered(true)}
+                onMouseLeave={()=> setIsHovered(false)}
+            >
                 <button
                 onClick={handleCheckboxChange}
                 className={`relative flex justify-between rounded-full border p-2 border-gray-300 cursor-pointer mr-5 ${
-                    isComplete ? 'bg-check-background' : ''
+                    todo.complete ? 'bg-check-background' : ''
                 }`}
                 >
-                {isComplete ? (
+                {todo.complete ? (
                     <img
                     src={check}
                     alt="Checkmark"
                     className='absolute inset-0 m-auto z-12'
                     />
                 ) : null}
-            </button>
-            <input
-                className={`outline-none h-10 w-full ${isComplete ? 'line-through' : ''} transition-all duration-500`} 
-                type="text"
-                defaultValue={todo.name}/>
-            <button 
-                onClick={ handleDelete }
-                className="absolute right-0 mt-2 mr-2 p-2 cursor-pointer">
-                <img src={cross} alt="Cross"/>
-            </button>
+                </button>
+                <input
+                    className={`outline-none h-10 w-full ${todo.complete ? 'line-through text-dark-grayish-blue' : ''} transition-all duration-500`} 
+                    type="text"
+                    defaultValue={todo.name}/>
+                {
+                    (isFocused || isHovered )&& (
+
+                    <button 
+                        onClick={ handleDelete }
+                        className="absolute right-0 mt-2 mr-2 p-2 cursor-pointer">
+                        <img src={cross} alt="Cross"/>
+                    </button>    
+                )}
             </li>
         )
     }
